4cbdc2a520c8a16cad5520db0591a7ecb6aa3f5a
qms1406 <qms1406@gmail.com>
2025-04-17 08:25:08 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,15 @@
+noise = 0
+light2 = 0
+led.enable(False)
+strip = neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB)
+
+def on_forever():
+    global light2, noise
+    light2 = smarthome.read_light_intensity(AnalogPin.P3)
+    if light2 < 50:
+        noise = smarthome.read_noise(AnalogPin.P2)
+        if noise > 70:
+            strip.show_color(neopixel.colors(NeoPixelColors.WHITE))
+            basic.pause(10000)
+            strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+basic.forever(on_forever)
